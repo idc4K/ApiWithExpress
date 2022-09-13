bb8944d79479739615ff5211d50daffc0e114b07
christ <christikonga83@gmail.com>
2022-09-13 16:40:27 +0200

    backend

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,20 @@ const express = require('express');
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
 const app = express();
+const userRoutes = require('./routes/user.routes');
 
+
+
+
+
+
+
+//routes
+app.use('/api/user', userRoutes);
+
+
+
+// server
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`);
 });
