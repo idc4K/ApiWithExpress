032eafe5e3e4337a01339d52872ae87e8a81faaf
christ <christikonga83@gmail.com>
2022-09-13 18:47:32 +0200

    backend

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema(
     {
       name: {
@@ -24,6 +26,10 @@ const userSchema = new mongoose.Schema(
         max: 1024,
         minlength: 6
       },
+      picture:{
+        type:String,
+        default: ""
+      },
       bio:{
         type:String,
         max:1024
@@ -43,6 +49,7 @@ const userSchema = new mongoose.Schema(
     }
 );
 
+//Avant de suvegarder dans la BD crypter password
 
 const UserModel = mongoose.model("user", userSchema);
 
