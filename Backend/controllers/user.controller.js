cf8004b4cb8bca76851b339cd8cc5be90bf63596
christ <christikonga83@gmail.com>
2022-09-19 16:35:24 +0200

    crud users

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -10,8 +10,9 @@ module.exports.UserInfos = async (req,res) =>{
    console.log(req.params);
    if(!ObjectID.isValid(req.params.id))
     return res.status(400).send('ID unknown :' + req.params.id);
+
    UserModel.findById(req.params.id,(err,docs) =>{
-    if(err) res.send(docs);
+    if(!err) res.send(docs);
     else console.log('ID unknown : '+err);
    }).select('-password');
 };
