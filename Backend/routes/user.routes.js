32e0c27636851e0fce0409ced9bc98e34405d083
christ <christikonga83@gmail.com>
2022-09-19 15:48:49 +0200

    crud users

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller'); 
-
+const userController = require('../controllers/user.controller');
 //Register Users 
 router.post('/register', authController.signUp);
 
+//Crud Users
+router.post('/',userController.GetAllUsers);
 module.exports = router;
