2b4175ae9c1c467cdc007a2d7017542fea702413
christ <christikonga83@gmail.com>
2022-09-20 16:06:42 +0200

    put users

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,11 +1,14 @@
 const UserModel = require('../models/user.model');
 const ObjectID = require('mongoose').Types.ObjectId;
 
+
+//GET ALL USERS
 module.exports.GetAllUsers = async (req,res) =>{
     const users = await UserModel.find().select('-password');
     res.status(200).json(users);
 };
 
+//GET USER BY ID
 module.exports.UserInfos = async (req,res) =>{
    console.log(req.params);
    if(!ObjectID.isValid(req.params.id))
@@ -15,4 +18,29 @@ module.exports.UserInfos = async (req,res) =>{
     if(!err) res.send(docs);
     else console.log('ID unknown : '+err);
    }).select('-password');
+};
+//UPDATE USER
+module.exports.UpdateUser = async (req,res) =>{
+    if(!ObjectID.isValid(req.params.id))
+        return res.status(400).send('ID unknown :' + req.params.id);
+    try{
+        await UserModel.findOneAndUpdate(
+            {_id: req.params.id},
+            {
+                $set:{
+                    bio:req.body.bio
+                }
+            },
+            {new:true,upsert:true,setDefaultsOnInsert:true},
+            (err,docs) =>{
+                if(!err) return res.send(docs);
+                if(err) return res.status(5000).send({message:err});
+            }
+
+        );
+        
+    }
+    catch(err){
+        return res.status(500).send({message:err});
+    }
 };
